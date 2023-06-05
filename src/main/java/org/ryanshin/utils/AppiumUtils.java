0635a55743ddb4947b2854ee143d0512491f3fe9
Ryan Shin <ryanminjaeshin@gmail.com>
2023-06-06 00:11:35 +0900

    add getScreenshot method

--- src/main/java/org/ryanshin/utils/AppiumUtils.java
+++ src/main/java/org/ryanshin/utils/AppiumUtils.java
@@ -1,8 +1,12 @@
 package org.ryanshin.utils;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
 
+import org.openqa.selenium.OutputType;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.io.FileHandler;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -26,6 +30,16 @@ public class AppiumUtils {
 	public void waitForElementToAppear(WebElement ele) {
 		WebDriverWait wait = new WebDriverWait(androidDriver, Duration.ofSeconds(5));
 		wait.until(ExpectedConditions.attributeContains((ele), "text", "Cart"));
+	}
+	
+	public String getScreenshot(String testCaseName, AppiumDriver androidDriver) throws IOException {
+		File source = androidDriver.getScreenshotAs(OutputType.FILE);
+		String destinationFile = System.getProperty("user.dir")+"//reports"+testCaseName+".png";
+		FileHandler.copy(source, new File(destinationFile));
+		return destinationFile;
+		// 1. capture and place in a folder
+		// 2. extent report pick file and attach to report
+		
 	}
 
 }
