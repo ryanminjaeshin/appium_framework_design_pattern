bb1d52fb2618da481603014c95e6f09c034fe738
Ryan Shin <ryanminjaeshin@gmail.com>
2023-06-05 21:46:31 +0900

    typo

--- src/main/java/org/ryanshin/pageObjects/android/CartPage.java
+++ src/main/java/org/ryanshin/pageObjects/android/CartPage.java
@@ -1,21 +1,23 @@
 package org.ryanshin.pageObjects.android;
 
 import org.openqa.selenium.support.PageFactory;
+import org.ryanshin.utils.AndroidActions;
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
 
-public class CartPage {
+public class CartPage extends AndroidActions {
 	
 	
 	//Driver invocation.
 	AndroidDriver androidDriver;
 
-	public ProductCatalog(AndroidDriver androidDriver) {
+	public CartPage(AndroidDriver androidDriver) {
 		super(androidDriver);
 		this.androidDriver = androidDriver;
 		PageFactory.initElements(new AppiumFieldDecorator(androidDriver), this);
 		
 	}
+
 
 }
