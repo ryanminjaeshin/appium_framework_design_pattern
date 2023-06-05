c787cabb5e8c63fa55571661236b164694d6da2d
Ryan Shin <ryanminjaeshin@gmail.com>
2023-06-05 18:09:35 +0900

    add methods

--- src/main/java/org/ryanshin/pageObjects/android/FormPage.java
+++ src/main/java/org/ryanshin/pageObjects/android/FormPage.java
@@ -2,17 +2,19 @@ package org.ryanshin.pageObjects.android;
 
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
+import org.ryanshin.utils.AndroidActions;
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.pagefactory.AndroidFindBy;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
 
-public class FormPage {
+public class FormPage extends AndroidActions {
 	
 	AndroidDriver androidDriver;
 	
 	public FormPage(AndroidDriver androidDriver) {
 		
+		super(androidDriver); // call parent class = AndroidActions
 		this.androidDriver=androidDriver;
 		
 		// this combine AndroidFindBy and androidDriver
